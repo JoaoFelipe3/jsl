99706f3fe01583a7e54ae6b0b9dada247339b22e
JoaoFelipe3 <ccristiany339@gmail.com>
2025-05-19 19:41:34 -0300

    Can now run files + good error messages

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,34 @@ mod result;
 mod run;
 mod value;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let code = fs::read_to_string("test.jsl")?;
+use result::*;
+
+fn main_helper(input_file: String) -> JSLResult<()> {
+    let code = fs::read_to_string(input_file).or(Err(JSLError {
+        msg: "could not read file".into(),
+    }))?;
     let ast: parse::AST;
     match run::gen_ast_from_code(code.as_str()) {
         Ok(good) => ast = good,
-        Err(e) => panic!("{}", e.msg),
+        Err(e) => return Err(e),
     }
-    let result = run::run_ast(ast, &mut vec![], &mut HashMap::new());
-    if let Some(error) = result.err() {
-        println!("{}", error.msg);
+    run::run_ast(ast, &mut vec![], &mut HashMap::new())
+}
+
+fn print_error(error: String) {
+    println!("\x1b[1;31merror:\x1b[0m {error}");
+}
+
+fn main() {
+    let mut args: Vec<String> = std::env::args().collect();
+    args.remove(0);
+    if args.len() < 1 {
+        print_error("expected jsl file as argument".into());
+        return;
     }
-    Ok(())
+    let file = args[0].clone();
+    let res = main_helper(file);
+    if let Some(err) = res.err() {
+        print_error(err.msg);
+    }
 }
